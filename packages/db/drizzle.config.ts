2e1cc54b4569c8e1214b6f708f8fe5c5afe0364c
zaidkhatri-dev <zaid.abdulgani.khatri@gmail.com>
2026-09-08 22:47:46 +0530

    feat: add schema definitions

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './drizzle',
-  schema: './schema/schema.ts',
+  schema: './schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
--- packages/db/schema.ts
+++ packages/db/schema.ts
@@ -1,1 +1,69 @@
-// Schemas
+import { pgTable, varchar, uuid, timestamp, pgEnum, uniqueIndex, integer, check } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import { boolean } from "drizzle-orm/cockroach-core";
+
+const memberRoles = pgEnum("member_role", ["admin", "member"]);
+
+export const users = pgTable("users", {
+    id: uuid("id").primaryKey().default(sql`uuidv7()`),
+    email: varchar("email", { length: 255 }).notNull(),
+    username: varchar("username", { length: 255 }),
+    profilePictureUrl: varchar("profile_picture_url", { length: 255 }),
+    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+    updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+}, (table) => [
+    uniqueIndex('email_lower_idx').on(sql`lower(${table.email})`) 
+]);
+
+export const organizations = pgTable("organizations", {
+    id: uuid("id").primaryKey().default(sql`uuidv7()`),
+    name: varchar("name", { length: 255 }).notNull(),
+    description: varchar("description", { length: 1000 }),
+    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+    updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+});
+
+export const membership = pgTable("membership", {
+    id: uuid("id").primaryKey().default(sql`uuidv7()`),
+    userId: uuid("user_id").notNull().references(() => users.id, { onDelete: "cascade" }),
+    organizationId: uuid("organization_id").notNull().references(() => organizations.id, { onDelete: "cascade" }),
+    role: memberRoles("role").notNull().default("member"),
+    accepted: boolean("accepted").notNull().default(false),
+    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+    updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+}, (table) => [
+    uniqueIndex('user_org_idx').on(table.userId, table.organizationId)
+]);
+
+export const boards = pgTable("boards", {
+    id: uuid("id").primaryKey().default(sql`uuidv7()`),
+    title: varchar("title", { length: 255 }).notNull(),
+    description: varchar("description", { length: 1000 }),
+    organizationId: uuid("organization_id").notNull().references(() => organizations.id, { onDelete: "cascade" }),
+    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+    updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+});
+
+export const sections = pgTable("sections", {
+    id: uuid("id").primaryKey().default(sql`uuidv7()`),
+    title: varchar("title", { length: 255 }).notNull(),
+    boardId: uuid("board_id").notNull().references(() => boards.id, { onDelete: "cascade" }),
+    order: integer("order").notNull().default(0),
+    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+    updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+}, (table) => [
+    check("order_check", sql`${table.order} >= 0`)
+]);
+
+export const issues = pgTable("issues", {
+    id: uuid("id").primaryKey().default(sql`uuidv7()`),
+    title: varchar("title", { length: 255 }).notNull(),
+    description: varchar("description", { length: 1000 }),
+    sectionId: uuid("section_id").notNull().references(() => sections.id, { onDelete: "restrict" }),
+    boardId: uuid("board_id").notNull().references(() => boards.id, { onDelete: "cascade" }),
+    order: integer("order").notNull().default(0),
+    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+    updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+}, (table) => [
+    check("order_check", sql`${table.order} >= 0`)
+]);
