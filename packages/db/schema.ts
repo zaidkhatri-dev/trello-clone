90bdd9acf5b9efb94a7be497bd066a4b091b4c49
zaidkhatri-dev <zaid.abdulgani.khatri@gmail.com>
2026-09-09 11:56:17 +0530

    refactor: replace repeated timestamp schema fields with a single timestamp object

--- packages/db/schema.ts
+++ packages/db/schema.ts
@@ -1,16 +1,19 @@
-import { pgTable, varchar, uuid, timestamp, pgEnum, uniqueIndex, integer, check, text } from "drizzle-orm/pg-core";
+import { pgTable, varchar, uuid, timestamp, pgEnum, uniqueIndex, integer, check, text, boolean } from "drizzle-orm/pg-core";
 import { sql } from "drizzle-orm";
-import { boolean } from "drizzle-orm/cockroach-core";
 
 export const memberRoles = pgEnum("member_role", ["admin", "member"]);
+
+const timestamps = {
+    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+    updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+}
 
 export const users = pgTable("users", {
     id: uuid("id").primaryKey().default(sql`uuidv7()`),
     email: varchar("email", { length: 255 }).notNull(),
     username: varchar("username", { length: 255 }),
     profilePictureUrl: varchar("profile_picture_url", { length: 255 }),
-    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-    updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+    ...timestamps
 }, (table) => [
     uniqueIndex('email_lower_idx').on(sql`lower(${table.email})`) 
 ]);
@@ -19,8 +22,7 @@ export const organizations = pgTable("organizations", {
     id: uuid("id").primaryKey().default(sql`uuidv7()`),
     name: varchar("name", { length: 255 }).notNull(),
     description: varchar("description", { length: 1000 }),
-    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-    updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+    ...timestamps
 });
 
 export const membership = pgTable("membership", {
@@ -29,8 +31,7 @@ export const membership = pgTable("membership", {
     organizationId: uuid("organization_id").notNull().references(() => organizations.id, { onDelete: "cascade" }),
     role: memberRoles("role").notNull().default("member"),
     accepted: boolean("accepted").notNull().default(false),
-    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-    updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+    ...timestamps
 }, (table) => [
     uniqueIndex('user_org_idx').on(table.userId, table.organizationId)
 ]);
@@ -40,8 +41,7 @@ export const boards = pgTable("boards", {
     title: varchar("title", { length: 255 }).notNull(),
     description: varchar("description", { length: 1000 }),
     organizationId: uuid("organization_id").notNull().references(() => organizations.id, { onDelete: "cascade" }),
-    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-    updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+    ...timestamps
 });
 
 export const sections = pgTable("sections", {
@@ -49,8 +49,7 @@ export const sections = pgTable("sections", {
     title: varchar("title", { length: 255 }).notNull(),
     boardId: uuid("board_id").notNull().references(() => boards.id, { onDelete: "cascade" }),
     order: integer("order").notNull().default(0),
-    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-    updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+    ...timestamps
 }, (table) => [
     check("order_check", sql`${table.order} >= 0`)
 ]);
@@ -62,8 +61,7 @@ export const issues = pgTable("issues", {
     sectionId: uuid("section_id").notNull().references(() => sections.id, { onDelete: "restrict" }),
     boardId: uuid("board_id").notNull().references(() => boards.id, { onDelete: "cascade" }),
     order: integer("order").notNull().default(0),
-    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-    updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+    ...timestamps
 }, (table) => [
     check("order_check", sql`${table.order} >= 0`)
 ]);
@@ -73,8 +71,7 @@ export const comments = pgTable("comments", {
     content: text("content").notNull(),
     commentedById: uuid("commented_by_id").notNull().references(() => users.id, { onDelete: "cascade" }),
     issueId: uuid("issue_id").notNull().references(() => issues.id, { onDelete: "cascade" }),
-    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-    updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+    ...timestamps
 }, (table) => [
     check("content_not_empty", sql`TRIM(${table.content}) <> ''`)
 ]);
