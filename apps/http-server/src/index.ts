385666f24e190beb7083a1441546795ac3e0ab16
zaidkhatri-dev <zaid.abdulgani.khatri@gmail.com>
2026-09-13 07:52:52 +0530

    refactor: validate env variables before express app loads

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,6 +1,7 @@
 import "dotenv/config"
-import app from "./app";
 import { env } from "@config/env";
+
+import app from "./app";
 import { GRACEFUL_SHUTDOWN_TIMEOUT } from "@constants/app";
 import { isShuttingDown, setShuttingDown } from "./shutdown";
 import { closeDBConnection } from "db/client";
